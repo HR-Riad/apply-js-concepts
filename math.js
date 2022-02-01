bf91eb4a01cbad0f0e1fc4c6224d2f3d79ff214a
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-02-02 00:56:07 +0600

    Add files via upload

---
+++ math.js
@@ -0,0 +1,28 @@
+/*const myNum = -5;
+const output = Math.abs(myNum);
+console.log(output);*/
+
+
+/*const myNum = 22.2222;
+const output = Math.ceil(myNum);
+console.log(output);*/
+
+/*const myNum = 22.2222;
+const output = Math.floor(myNum);
+console.log(output);*/
+
+/*const myNum = 22.2222;
+const output = Math.round(myNum);//round means nearest digit.
+console.log(output);*/
+
+
+/*const output = Math.random()*6;//random means show difirent values in same code.
+const rounded = Math.floor(output);
+console.log(rounded);*/
+
+// in for loop
+for (let i=0; i<=20; i++){
+    const output = Math.random()*6;
+const rounded = Math.round(output);
+console.log(rounded);
+}
