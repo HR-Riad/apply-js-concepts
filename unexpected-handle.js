0b91b5d9b29dc1f8f3bb9068d489e2800867259f
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-02-02 16:13:33 +0600

    Add files via upload

---
+++ unexpected-handle.js
@@ -0,0 +1,16 @@
+function fibonacciSeries(num){
+    if (typeof num != 'number')
+    {
+        return'please give a number';
+    }
+    if (num<2){
+        return 'please enter a positive number greater than 1';
+    }
+    const fibo = [0,1];
+    for (let i =2 ; i<=num; i++){
+        fibo[i]=fibo[i-1]+fibo[i-2];
+    }
+    return fibo;
+}
+const fiboSeries= fibonacciSeries (22);
+console.log(fiboSeries);
